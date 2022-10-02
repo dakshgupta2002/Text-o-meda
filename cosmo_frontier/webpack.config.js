49cc753556b0b316dc073c74b68463fc0f541754
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-10-02 12:57:04 +0530

    dev wont start

--- cosmo_frontier/webpack.config.js
+++ cosmo_frontier/webpack.config.js
@@ -1,19 +1,26 @@
-const copyPlugin = require("copy-webpack-plugin");
-module.exports = {
-  entry: {
-    "js/script.js": "./src/js/main.js",
-  },
-  output: {
-    path: __dirname + "/dist",
-    filename: "[name]",
-  },
-  resolve: { extensions: [".js"] },
-  plugins: [
-    new copyPlugin({
-      patterns: [
-        { from: "html", context: "src" },
-        { from: "assets", context: "src", to: "assets" },
-      ],
-    }),
-  ],
+import copyPlugin from "copy-webpack-plugin";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const entry = {
+  "js/script.js": "./src/js/main.js",
 };
+
+export const output = {
+  path: __dirname + "/dist",
+  filename: "[name]",
+};
+
+export const resolve = { extensions: [".js"] };
+
+export const plugins = [
+  new copyPlugin({
+    patterns: [
+      { from: "html", context: "src" },
+      { from: "assets", context: "src", to: "assets" },
+    ],
+  }),
+];
